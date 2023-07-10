f97aa4e82ec8e1eefea32a4d8b03bff01a3e077f
Suzanne <suzanneschuber@gmail.com>
2023-07-10 13:57:57 -0700

    add CheckType method

--- ShapeTracker/Models/Triangle.cs
+++ ShapeTracker/Models/Triangle.cs
@@ -29,6 +29,11 @@ namespace ShapeTracker.Models
         {
             _side3 = newValue;
         }
+
+        public string CheckType()
+        {
+            return "not a triangle";
+        }
     }
 }
 
